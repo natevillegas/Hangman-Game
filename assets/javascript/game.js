b69286b739b3170a6dca386b0d703c532adc15af
Nate Villegas <natemv@gmail.com>
2017-05-06 22:54:39 -0700

    completed initial html layout and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+// list of words to play with
+var words = ["skrt skrt","dab","raindrop","droptop","yeah yeah yeah", "dat way", "aye", "bao","whoop","woo", "bad and boujee","culture","versace","band"];
+// creates an emtpy array of guesses
+var wrongGuess = [];
+// random generator so user gets a new word every new game
+var nextWord = words[Math.floor(Math.random()*words.length)];
+
+for (i = 0, i < words.length, i++) {
+	if (words[i] )
+}
+
+if (userInput == "L") {
+	nextWord
+}
+
+
+
+
+
+// function that gets user inputted letter
+document.onkeyup = function() {
+	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+	alert(userInput);
+}
+
+
+var html = "#"
+
+// prints 
+document.querySelector('#game').innterHTML	= html;
+
+
+
+
+
+
+
+//lone function
+
+wrongGuess.push(userInput);
+
